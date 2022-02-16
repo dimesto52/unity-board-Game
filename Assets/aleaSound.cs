3f0606fd36577a40dcc32b62d1f20eabeea89bdd
dimesto52 <vincent.loris@gmail.com>
2022-02-16 17:41:00 +0100

    random multi sound

---
+++ Assets/aleaSound.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aleaSound : MonoBehaviour
+{
+
+    public AudioSource source
+    {
+        get
+        {
+            return this.GetComponent<AudioSource>();
+        }
+    }
+
+    public List<AudioClip> clips = new List<AudioClip>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int id = Random.Range(0, clips.Count);
+        source.clip = clips[id];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
